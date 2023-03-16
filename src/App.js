d236032d17c090143534ac5a3c71f2d4dad4edb9
Yash Singh <yashsingh9009670@gmail.com>
2023-03-16 14:50:59 +0530

    gh pages Deployed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Navbar } from "./components/Navbar";
 import { Sidebar } from "./components/Sidebar";
@@ -8,24 +8,25 @@ import { NewUser } from "./pages/NewUser";
 function App() {
   return (
     <>
-      <div className="w-screen dark:bg-slate-900 min-h-screen">
-        <div className="flex max-w-screen-2xl min-h-[100svh] m-auto dark:bg-slate-900 dark:text-white">
-          <Sidebar/>
-          <div className="w-full lg:w-5/6">
-            <Navbar/>
-            <div className="overflow-y-scroll h-[92vh] p-4">
-              <Routes>
-                <Route exact path="/" element={<Home/>}/>
-                <Route exact path="/users" element={<Users/>}/>
-                <Route exact path="/users/userProfile" element={<UserProfile/>}/>
-                <Route exact path="/newUser" element={<NewUser/>}/>
-              </Routes>
+      <BrowserRouter basename="/Admin">
+        <div className="w-screen dark:bg-slate-900 min-h-screen">
+          <div className="flex max-w-screen-2xl min-h-[100svh] m-auto dark:bg-slate-900 dark:text-white">
+            <Sidebar />
+            <div className="w-full lg:w-5/6">
+              <Navbar />
+              <div className="overflow-y-scroll h-[92vh] p-4">
+                <Routes>
+                  <Route exact path="/" element={<Home />} />
+                  <Route path="/users" element={<Users />} />
+                  <Route path="/users/userProfile" element={<UserProfile />} />
+                  <Route path="/newUser" element={<NewUser />} />
+                </Routes>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </BrowserRouter>
     </>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {BrowserRouter} from "react-router-dom";
 import App from './App';
 import StateContext from './context/StateContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename='/Admin'>
     <StateContext>
     <App />
     </StateContext>
-    </BrowserRouter>
 );
